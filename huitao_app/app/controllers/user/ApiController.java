982232d82754b80e8d144a1ac815938977277e8a
chenzhongyi <765485378@qq.com>
2019-03-04 11:19:42 +0800

    m

--- huitao_app/app/controllers/user/ApiController.java
+++ huitao_app/app/controllers/user/ApiController.java
@@ -8,7 +8,8 @@ public class ApiController extends BaseController {
     @Before(priority = 100, unless = {
 			"user.Application.index", "user.Application.version", "user.Application.download",
             "user.Application.configData", "user.Application.areaData", "user.Application.qiniuUptoken", "user.Application.push",
-            "user.PersonController.captcha", "user.PersonController.login", "user.PersonController.forgetPassword"
+            "user.PersonController.captcha", "user.PersonController.login", "user.PersonController.forgetPassword",
+            "user.CategoryController.list", "user.NewsController.list", "user.NewsController.info"
     })
     public static void access() {
         accesstoken();
